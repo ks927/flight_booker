5ecac22161e335a23430f64e9cc4e351bb9a1914
KSheehan <ksheehan927@gmail.com>
2017-07-11 19:45:35 -0400

    Seed database with flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,15 @@ Airport.delete_all
     Airport.create!(name: name)
 end
 
-Flights.delete_all
+Flight.delete_all
+Airport.all.each do |from_airport|
+    Airport.all.each do |to_airport|
+      unless from_airport == to_airport
+        time = rand(1..10)
+        Flight.create!(from_airport_id: from_airport.id,
+                        to_airport_id: to_airport.id,
+                        date: (Time.now + time.weeks),
+                        duration: 60 * time)
+      end
+    end
+end
